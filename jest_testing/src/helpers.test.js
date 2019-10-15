85d370e70ec8d9cc3750ac3084a7b8ecd7530bab
derpWanda <derpWanda@gmail.com>
2019-10-15 11:13:13 -0500

    added CRA

--- jest_testing/src/helpers.test.js
+++ jest_testing/src/helpers.test.js
@@ -4,13 +4,15 @@ const help = require('./helpers');
 //'describe' is a global variable injected by jest
 
 //'describe' blocks are necessary and can be nested to group tests
-describe('sum function', () => {
-    // write your test inside the describe block
-    it('sums two integers', () => {
-        const expected = 3;
-        const actual = help.sum(1, 2);
-        //when we run the sum function with these input (1 and 2)
-        //we expect it to be 3
-        expect(actual).toBe(expected)
+describe('helpers module', () => {
+    describe('sum function', () => {
+        // write your test inside the describe block
+        it('sums two integers', () => {
+            const expected = 3;
+            const actual = help.sum(1, 2);
+            //when we run the sum function with these input (1 and 2)
+            //we expect it to be 3
+            expect(actual).toBe(expected)
+        })
     })
 })
