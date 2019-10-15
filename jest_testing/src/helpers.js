5242aaf431253b15fcf34d3f574bc0aa9f53abf8
derpWanda <derpWanda@gmail.com>
2019-10-15 11:20:53 -0500

    add comments, updated test name; add 2nd function

--- jest_testing/src/helpers.js
+++ jest_testing/src/helpers.js
@@ -1,5 +1,10 @@
 //the function we are testing
 //exports allows us to export the function
-exports.sum = function (a, b) {
+
+exports.sum2 = function (a, b) {
     return a + b;
+}
+
+exports.sumMore = function (...args) {
+    return args.reduce((acc, e) => acc + e)
 }
--- jest_testing/src/helpers.test.js
+++ jest_testing/src/helpers.test.js
@@ -5,13 +5,20 @@ const help = require('./helpers');
 
 //'describe' blocks are necessary and can be nested to group tests
 describe('helpers module', () => {
-    describe('sum function', () => {
+    describe('sum two numbers', () => {
         // write your test inside the describe block
         it('sums two integers', () => {
             const expected = 3;
-            const actual = help.sum(1, 2);
+            const actual = help.sum2(1, 2);
+            //create an assertion
             //when we run the sum function with these input (1 and 2)
             //we expect it to be 3
+            expect(actual).toBe(expected)
+        })
+
+        it('can add three numbers', () => {
+            const expected = 6;
+            const actual = help.sumMore(1, 2, 3);
             expect(actual).toBe(expected)
         })
     })
