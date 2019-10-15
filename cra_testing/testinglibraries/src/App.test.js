1bd3f2f6e8d68cb62a7daba9da99d6824c222bd7
derpWanda <derpWanda@gmail.com>
2019-10-15 13:57:41 -0500

    adeed working tests

--- cra_testing/testinglibraries/src/App.test.js
+++ cra_testing/testinglibraries/src/App.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+
+// 
+it('App renders without crashing', () => {
+  const wrapper = rtl.render(<App />);
+  console.log(wrapper.debug())
+
+  const element = wrapper.queryByText(/hello World/i);
+  expect(element).toBeInTheDocument(); //DOM //jest matcher
+  expect(element).toBeTruthy(); //jest-dom matcher
+  expect(element).toBeVisible(); //DOM //jest-dom matcher
 });
+
+it('span renders the text Hello World without crashing', () => {
+  const wrapper = rtl.render(
+    <span className="greet">hello World</span>
+  );
+});
+
+/*
+// dom-testing-library queries
+// SELECT DOM NODES USING QUERIES
+// There are many queries available, for single and multiple elements, and with // three different prefixes: query, get and find
+it('renders "all about kittens 1" text', () => {
+  const wrapper = rtl.render(<App foo="you may inject props!" />);
+  console.log(wrapper.debug())
+  // IMPORTANT
+  // wrapper.queryByText() returns either the node, or null:
+  const hasKittensText = wrapper.queryByText(/all about kittens/i);
+  // expect(hasKittensText).toBeInTheDocument();
+  // wrapper.getByText() is an alternative to wrapper.queryByText()
+  expect(wrapper.getByText(/all about kittens/i));
+});
+
+it('renders "all about kittens 2" text', () => {
+  const wrapper = rtl.render(<App />);
+  expect(wrapper.getByLabelText(/All About Kittens/i));
+});
+
+
+ */
